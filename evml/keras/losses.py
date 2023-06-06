1f2418f96ffe7289868c299bad1f283efb38028b
Dhamma Kimpara <dkimpara@crhtc62.hpc.ucar.edu>
2023-06-06 15:47:41 -0600

    draft of dirichlet with informed prior loss

--- evml/keras/losses.py
+++ evml/keras/losses.py
@@ -107,3 +107,63 @@ def GaussianNLL(y, y_pred, reduce=True):
     logprob = -tf.math.log(sigma) - 0.5*tf.math.log(2*np.pi) - 0.5*((y-mu)/sigma)**2
     loss = tf.reduce_mean(-logprob, axis=ax)
     return tf.reduce_mean(loss) if reduce else loss
+
+
+class DirichletInformedPriorLoss(tf.keras.losses.Loss):
+    def __init__(self, callback=False, name="dirichletIP"):
+
+        super().__init__()
+        self.callback = callback
+        self.__name__ = name
+
+    def KL(self, alpha, prior_dist):
+        beta = prior_dist
+        S_alpha = tf.reduce_sum(alpha, axis=1, keepdims=True)
+        S_beta = tf.reduce_sum(beta, axis=1, keepdims=True)
+        
+        A = tf.math.lgamma(S_alpha) - tf.math.lgamma(S_beta) #ln Gamma(K) 
+        
+        B = (
+            tf.reduce_sum(tf.math.lgamma(beta), axis=1 keepdims=True) 
+             - tf.reduce_sum(tf.math.lgamma(alpha), axis=1, keepdims=True)
+            )
+
+        dg0 = tf.math.digamma(S_alpha)
+        dg1 = tf.math.digamma(alpha)
+
+        kl = (
+            tf.reduce_sum((alpha - beta) * (dg1 - dg0), axis=1, keepdims=True)
+            + A
+            + B
+        )
+        return kl
+
+    def __call__(self, y, output, sample_weight = None):
+        '''
+        note: need to ensure that the informed prior sums to a known constant (e.g. 1 or K) in order
+        to derive uncertainty value at inference time
+        '''
+        
+        evidence = tf.nn.relu(output)
+        
+        y_len = int(y.shape[1] / 2)
+        prior_dist = y[: , y_len: ] #2nd half of y
+        y = y[: ,  :y_len] #first half of y
+
+        alpha = evidence + prior_dist
+
+        S = tf.reduce_sum(alpha, axis=1, keepdims=True)
+        m = alpha / S
+
+        A = tf.reduce_sum((y - m) ** 2, axis=1, keepdims=True)
+        B = tf.reduce_sum(
+            alpha * (S - alpha) / (S * S * (S + 1)), axis=1, keepdims=True
+        )
+
+        annealing_coef = tf.minimum(
+            1.0, self.callback.this_epoch / self.callback.annealing_coef
+        )
+        alpha_hat = y + (1 - y) * alpha
+        C = annealing_coef * self.KL(alpha_hat, prior_dist)
+        C = tf.reduce_mean(C, axis=1)
+        return tf.reduce_mean(A + B + C)
